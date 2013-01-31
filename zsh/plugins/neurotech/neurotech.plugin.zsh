a36fd3d10bf5e888780dae8b8d94d4038e634add
Tim Douglas <neurotech@gmail.com>
2013-01-31 20:18:47 +1100

    Improved deploy function

--- zsh/plugins/neurotech/neurotech.plugin.zsh
+++ zsh/plugins/neurotech/neurotech.plugin.zsh
@@ -1,24 +1,25 @@
-# Colours
+# Colour Variables
 RED="\033[1;31m"
 MAGENTA="\033[1;35m"
 GREEN="\033[1;32m"
 YELLOW="\033[1;33m"
 RESET="\033[m"
 
-alias ec='ssh -p 2222 timd@184.173.236.33'
+# SSH to HostGator
 alias gator='ssh -p 2222 timd@184.173.236.33'
 
-alias deploygator='middleman build ~/Dropbox/projects/electric-clouds/middleman/ | rsync -avzr --progress --rsh="ssh -p 2222" ~/Dropbox/projects/electric-clouds/middleman/build/ timd@184.173.236.33:/home/timd/public_html/mm/'
+# Build and Sync to HostGator
+function deploygator() {
+  boomerang=$PWD
 
-alias deploybonsai='rsync -avzr --progress --rsh="ssh -p 2222" ~/Dropbox/projects/electric-clouds/bonsai/output/ timd@184.173.236.33:/home/timd/public_html/bonsai/'
-
-function deploygatorr() {
-  echo "${RED}Switching to working directory and building with ${GREEN}middleman...${RESET}"
+  echo "Switching to working directory and building with ${GREEN}middleman${RESET}..."
   cd ~/Dropbox/projects/electric-clouds/middleman/
   middleman build
 
   echo "${YELLOW}Deploying with rsync...${RESET}"
   rsync -avzr --progress --rsh="ssh -p 2222" ~/Dropbox/projects/electric-clouds/middleman/build/ timd@184.173.236.33:/home/timd/public_html/mm/
 
+  cd ${boomerang}
+  
   echo "${GREEN}Done.${RESET}"
 }
