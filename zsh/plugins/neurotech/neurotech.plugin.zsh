9da80bf987990825142a7deb8c91948d54f6bbcb
Tim Douglas <neurotech@gmail.com>
2013-02-22 15:56:28 +1100

    Added script to monitor Rosebank services.

--- zsh/plugins/neurotech/neurotech.plugin.zsh
+++ zsh/plugins/neurotech/neurotech.plugin.zsh
@@ -1,7 +1,8 @@
 # Colour Variables
 RED="\033[1;31m"
-MAGENTA="\033[1;35m"
 GREEN="\033[1;32m"
+BLUE="\033[1;34m"
+MAGENTA="\033[1;35m"
 YELLOW="\033[1;33m"
 RESET="\033[m"
 
@@ -28,4 +29,24 @@ function deploygator() {
   cd ${boomerang}
 
   echo "${GREEN}Done.${RESET}"
+}
+
+# REAL Status
+function rbcup() {
+  realupdown=$(curl -sL -w "%{http_code}" "http://real.rosebank.nsw.edu.au/login/index.php" -o /dev/null)
+  edumateupdown=$(curl -sL -w "%{http_code}" "https://edumate.rosebank.nsw.edu.au/rosebank/pages/main/login.php" -o /dev/null)
+
+  if [ $realupdown = '200' ]
+    then
+      echo "${GREEN}REAL is up!${RESET}"
+    else
+      echo "${RED}Error: ${BLUE}${realupdown}${RESET}"
+  fi
+
+  if [ $edumateupdown = '200' ]
+    then
+      echo "${GREEN}Edumate is up!${RESET}"
+    else
+      echo "${RED}Error: ${BLUE}${edumateupdown}${RESET}"
+  fi
 }
